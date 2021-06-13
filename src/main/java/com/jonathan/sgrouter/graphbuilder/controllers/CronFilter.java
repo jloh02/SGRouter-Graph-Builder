ae4ae531d7dc32afff25605033e55a002402aa0c
jloh02 <36648707+jloh02@users.noreply.github.com>
2021-06-13 18:50:21 +0800

    Updated cron handling

--- src/main/java/com/jonathan/sgrouter/graphbuilder/controllers/CronFilter.java
+++ src/main/java/com/jonathan/sgrouter/graphbuilder/controllers/CronFilter.java
@@ -1,6 +1,5 @@
 package com.jonathan.sgrouter.graphbuilder.controllers;
 
-import com.jonathan.sgrouter.graphbuilder.GraphBuilderApplication;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -8,6 +7,10 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.jonathan.sgrouter.graphbuilder.GraphBuilderApplication;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -19,11 +22,15 @@ public class CronFilter implements Filter {
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
       throws IOException, ServletException {
-    if (GraphBuilderApplication.config.appengineDeployment) {
-      HttpServletRequest req = (HttpServletRequest) request;
+    HttpServletRequest req = (HttpServletRequest) request;
+    String appengineHeader = req.getHeader("X-Appengine-Country");
+    GraphBuilderApplication.appengineDeployment = appengineHeader!=null;
+    log.debug(appengineHeader);
+    if (appengineHeader!=null && appengineHeader.equals("true")) {
       String cronHeader = req.getHeader("X-Appengine-Cron");
       if (cronHeader == null || !cronHeader.equals("true")) {
         log.warn("Invalid cron request: {}", req);
+        ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized to call this API");
         return;
       }
     }
